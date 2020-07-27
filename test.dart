baa3b6958979e25fa1534d5a4a6e134504c25187
Alfredo Rodriguez Siles <alfredo@jarscr.com>
2020-07-27 16:20:02 -0600

    'Paso 1'

---
+++ test.dart
@@ -0,0 +1,9 @@
+void main() {
+  
+   String enfermedad = 'Covid19';
+   int personas = 16360298;
+   double muertes = 650.918;
+  
+   print('El mundo esta luchando contra $enfermedad con $personas y muertes $muertes');
+  
+}
